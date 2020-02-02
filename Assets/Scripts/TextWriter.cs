df14dd5ca613f8b005b0c56a2099b9de15311f64
HassanAdham <36557564+HassanAdham@users.noreply.github.com>
2020-02-02 05:42:13 +0200

    menu paragraph

---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    private Text uiText;
+    private string textToWrite;
+    private int charIndex;
+    private float timePerChar;
+    private float timer;
+    private bool invisivleChar;
+
+    public void AddWriter(Text uiText, string textToWrite, float timePerChar, bool invisivleChar)
+    {
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerChar = timePerChar;
+        this.invisivleChar = invisivleChar;
+        charIndex = 0;
+    }
+
+    private void Update()
+    {
+        if(uiText != null)
+        {
+            timer -= Time.deltaTime;
+            while (timer <= 0f)
+            {
+                timer += timePerChar;
+                charIndex++;
+                string text = textToWrite.Substring(0, charIndex);
+                if(invisivleChar)
+                {
+                    text += "<color=#00000000>" + textToWrite.Substring(charIndex) + "</color>";
+                }
+                uiText.text = text;
+
+                if (charIndex >= textToWrite.Length)
+                {
+                    uiText = null;
+                    return;
+                }
+            }
+        }
+    }
+}
